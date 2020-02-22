4560119bb55310a1a22c191ff57d593e85066803
RamonaEJSpence <ramonaejspence@gmail.com>
2020-02-22 13:50:34 -0500

    just had a large hurdle with adding foreignkeys and making migrations. Got it figured out, now to add the html for mytools page

--- tools/models.py
+++ tools/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Tool(models.Model):
     powertool = 'powertool'
@@ -15,7 +18,8 @@ class Tool(models.Model):
     type = models.TextField(max_length = 255, choices = available_types)
     description = models.TextField(max_length = 255)
     borrowed = models.BooleanField()
-    owner = models.CharField(max_length = 255) ##foreign key
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
--- tools/urls.py
+++ tools/urls.py
@@ -5,7 +5,7 @@ app_name = 'tools'
 
 urlpatterns = [
     path('users/', views.UserView.as_view(), name = "users"),
-    path('mytools/', views.MyToolView.as_view(), name = "mytools"),
+    path('/mytools/', views.MyToolView.as_view(), name = "mytools"),
     path('tools/', views.ToolListView.as_view(), name='list'),
     path('', views.IndexView.as_view(), name = "home"),
 ]
